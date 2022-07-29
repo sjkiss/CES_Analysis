a89ce63b72d889cc5c5e387a3d8bb15cbe013b62
Simon Kiss <sjkiss@gmail.com>
2022-07-29 15:05:40 -0400

    Push at the end of July 29

--- R_Scripts/3_book_chapter.R
+++ R_Scripts/3_book_chapter.R
@@ -63,4 +63,90 @@ ces %>%
   labs(y="Percent", x="Election", title="Share Of Each Party's Electorate By Class",linetype="Vote")+theme(axis.text.x=element_text(angle=90))
 ggsave(here("Plots", "book_party_share_by_occupation.png"), width=8, height=4)
 
+ces$region2<-factor(ces$region2, levels=c("Atlantic", "Quebec", "Ontario", "West"))
+ces$income_tertile
+ces$traditionalism2
+ces %>% 
+  filter(election>1988&Period==0)->ces.1
+ces %>% 
+  filter(Period==1&election<2022)->ces.2
+table(ces.2$election)
+table(ces.1$election)
+ces$immi
+ndp1<-lm(ndp~region2+male+age+income_tertile+degree+catholic+no_religion+redistribution+market_liberalism+immigration_rates+traditionalism+`1993`+`1997`+`2000`, data=ces.1)
+ndp2<-lm(ndp~region2+male+age+income_tertile+degree+catholic+no_religion+redistribution+market_liberalism+immigration_rates+traditionalism+`2004`+`2006`+`2008`+`2011`+`2015`+`2019`, data=ces.2)
 
+lib1<-lm(liberal~region2+male+age+income_tertile+degree+catholic+no_religion+redistribution+market_liberalism+immigration_rates+traditionalism+`1993`+`1997`+`2000`, data=ces.1)
+lib2<-lm(liberal~region2+male+age+income_tertile+degree+catholic+no_religion+redistribution+market_liberalism+immigration_rates+traditionalism+`2004`+`2006`+`2008`+`2011`+`2015`+`2019`, data=ces.2)
+
+con1<-lm(conservative~region2+male+age+income_tertile+degree+catholic+no_religion+redistribution+market_liberalism+immigration_rates+traditionalism+`1993`+`1997`+`2000`, data=ces.1)
+con2<-lm(conservative~region2+male+age+income_tertile+degree+catholic+no_religion+redistribution+market_liberalism+immigration_rates+traditionalism+`2004`+`2006`+`2008`+`2011`+`2015`+`2019`, data=ces.2)
+model.list<-list(ndp1, ndp2, lib1, lib2, con1, con2)
+library(stargazer)
+stargazer(model.list, 
+          type="html", out=here("Tables", "book_table_1_ols_pre_post.html"))
+lookfor(ces19phone, "last")
+lookfor(ces19phone, "vote")
+ces19phone %>% 
+  select(occupation4, vote2 ) %>% 
+  filter(vote2)
+ces19phone$past_vote
+#USe ces19phone
+ces19phone %>% 
+  #WE need past_vote for 2015, vote for 2019 and occupation3 for the class variable that has the self-employed
+  select(past_vote, vote, occupation3) %>% 
+  #Convert to factors
+  as_factor() %>% 
+  #We are only interested in NDP voters from 2015. Which way did they go?
+  filter(past_vote=="NDP") %>% 
+  #WE want to know the counts of the 2015 NDP voters 2019 vote choice by occupation 
+  #So we form groups by occupation3 and vote
+  group_by(occupation3, vote) %>% 
+  #Count
+  summarize(n=n()) %>% 
+  #Form percents
+  #This should be the vote flows of 2015 NDP voters by occupation 
+  mutate(pct=n/sum(n)) %>% 
+  #WE don't want people who had no reported vote in 2019
+  filter(!is.na(vote)) %>% 
+  #We don't want people with other votes in 2019
+  filter(vote!="Other") %>% 
+  #This cleans up the occupation variable and combines the unskilled and skilled categories
+  mutate(occupation4=Recode(occupation3, "'Routine_Nonmanual'='Routine Non-Manual'; 'Skilled'='Working Class';
+                            'Unskilled'='Working Class'; 
+                            'Self_employed'='Self-Employed'", 
+                            levels=c("Managers", "Professional", "Self-Employed", "Routine Non-Manual", "Working Class"))) %>%  
+  #Graph
+  mutate(vote=fct_relevel(vote, "Bloc",  "Conservative", "Green", "Liberal", "NDP")) %>% 
+  ggplot(., aes(x=pct, fill=vote, y=vote))+geom_col()+facet_wrap(~occupation4)+xlim(c(0,0.6)) + 
+  scale_fill_manual(values=c("grey80", "grey70", "grey60", "grey50", "black"))+theme(legend.position="none")+labs(x="Percent", y="Vote", title="2019 Votes of 2015 NDP voters, by class")
+ggsave(here("Plots", "book_vote_flow.png"))
+
+ces19phone %>% 
+  select(occupation3, mip)->out19
+ces15phone %>% 
+  select(occupation3, mip)->out15
+out15$Election<-rep("2015",nrow(out15))
+out19$Election<-rep("2019", nrow(out19))
+out15 %>% 
+  bind_rows(out19) %>% 
+  as_factor() %>% 
+  mutate(occupation3=Recode(occupation3, "'Routine_Nonmanual'='Routine Non-Manual'; 'Skilled'='Working Class';
+                            'Unskilled'='Working Class'; 
+                            'Self_employed'='Self-Employed'", 
+                            levels=c("Managers", "Professional", "Self-Employed", "Routine Non-Manual", "Working Class")) ) %>% 
+mutate(Issue=Recode(mip, "'Jobs'='Jobs and Economy' ;
+                    'Economy'='Jobs and Economy'"))  %>% 
+  group_by(Election, occupation3, Issue) %>% 
+  summarize(n=n()) %>% 
+  mutate(Percent=(n/sum(n))*100) %>% 
+  arrange(Issue, occupation3) %>% 
+  group_by(Issue, occupation3) %>% 
+  mutate(Change=Percent-lag(Percent)) %>% 
+  #filter(Election==2019) %>% 
+  filter(str_detect(Issue, "Jobs|Environment|Immigration|Health|Energy")) %>% 
+  filter(!is.na(occupation3)) %>% 
+  ggplot(., aes(y=occupation3, x=Percent, fill=fct_relevel(Issue, "Environment", "Energy")))+
+  facet_grid(~Election)+geom_col(position="dodge")+labs(y="Class")+scale_fill_grey()
+ggsave(here("Plots", "book_mip_change.png"))
+  
