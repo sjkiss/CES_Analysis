60c3c14fc1276419c3ada3c5631c5acb2b2ad896
Simon Kiss <sjkiss@gmail.com>
2022-11-02 15:10:40 -0400

    Caught Recodes in 1993

--- R_Scripts/2_9_ces93_recode.R
+++ R_Scripts/2_9_ces93_recode.R
@@ -140,7 +140,8 @@ table(ces93$party_id)
 #### vote#### 
 #recode Vote (PESA4)
 look_for(ces93, "vote")
-ces93$vote<-Recode(ces93$PESA4, "1=2; 2=1; 3=3; 5=4; 4=2; else=NA")
+ces93$PESA4
+ces93$vote<-Recode(ces93$PESA4, "1=2; 2=1; 3=3; 5=4; 4=2; 0=0; else=NA")
 val_labels(ces93$vote)<-c(Other=0, Liberal=1, Conservative=2, NDP=3, Bloc=4, Green=5)
 #checks
 val_labels(ces93$vote)
