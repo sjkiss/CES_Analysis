b05412b7f78d7c5fda90ac5d09716bf124e3aba4
Simon Kiss <sjkiss@gmail.com>
2023-12-20 09:14:17 -0500

    election to ordered

--- R_Scripts/1_master_file_original_cesdata.R
+++ R_Scripts/1_master_file_original_cesdata.R
@@ -836,7 +836,9 @@ library(skimr)
 #This code is just deleting a bunch of statistics that are not usefulr
 my_skim<-skim_with(factor=sfl(ordered=NULL, n_unique=NULL), 
                    numeric = sfl(p0=NULL, sd=NULL,p25=NULL, p50=NULL, p75=NULL, p100=NULL,hist = NULL))
+ces$election<-factor(ces$election, ordered=T)
 ces %>% 
+ # filter(election!=2021) %>% 
   #group_by(election) %>% 
   my_skim() %>% 
   write.csv("Results/cesdata_original_descriptives.csv", row.names=F)
